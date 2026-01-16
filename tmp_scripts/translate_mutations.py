b6f6cc3da872d8f0ba70b1d8ee5a5e3070b507c6
yutiachen5 <yutiachen5-c@my.cityu.edu.hk>
2026-01-16 14:58:55 -0800

    fixed problem in heatmap for prevalence and uploaded processed input files for ingestion

--- tmp_scripts/translate_mutations.py
+++
@@ -1,8 +0,0 @@
-    # if any(nuc not in set("ACGT") for nuc in ref_codon):
-    #     ref_aa = get_possible_aas(ref_codon)
-    # else:
-    #     ref_aa = str(Seq(ref_codon).translate(table=1))
-    # if any(nuc not in set("ACGT") for nuc in alt_codon):
-    #     alt_aa = get_possible_aas(alt_codon)
-    # else:
-    #     alt_aa = str(Seq(alt_codon).translate(table=1))
